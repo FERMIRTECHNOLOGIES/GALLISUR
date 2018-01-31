c0baac834fd3a12dbb914a58fafa842fd30a3d3f
FERMIRTECHNOLOGIES <fermir@github.com>
2018-01-30 22:07:26 -0600

    p

--- Principal.php
+++ Principal.php
@@ -97,7 +97,7 @@
 	<div class="ui-widget">
 	<div class="ui-state-highlight ui-corner-all" style="padding: 0 .7em;">
 		<p><span class="ui-icon ui-icon-info" style="float: left; margin-right: .3em;"></span>
-		<strong>Mensaje!</strong> <p id="message">""</p> </p>
+		<strong>Mensaje!</strong> <p id="message">""</p>
 	</div>
 </div>
 </div>
